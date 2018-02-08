331994e114c680302e1caa8a8b48c6c0a6791e6c
sanath <sanath.kumar@Euromonitor.com>
2018-02-08 13:58:16 +0530

    changed the URI to return json

--- Deploy-NestedSolution.ps1
+++ Deploy-NestedSolution.ps1
@@ -2,11 +2,26 @@
 $location = 'Australia Southeast'
 $resourceGroupName = 'pluralsight-arm-nested'
 $resourceDeploymentSolutionName = 'pluralsight-arm-nested-deployment'
-$templateBaseUri = 'https://steamstorage01.blob.core.windows.net/resources'
+$templateBaseUri = 'https://raw.githubusercontent.com/sanathkumarshetty/Azure-ARM/master/'
 $templateFile = 'azureDeploy.json'
+
+
 $template = $templateBaseUri + '/' + $templateFile
 $templateParametersFile = 'azureDeploy.parameters.json'
 $templateParameters = $templateBaseUri + '/' + $templateParametersFile
+
+$tenantID = "dfcdef42-195d-4e0d-9808-2d488688da0e"  #ashoktenantID
+$SPNID = "275b388b-537d-4bfc-8859-f7b34444ca77" #ashokSPN
+$SPNSecret ="PG4LuPgK1aNZpnErJq+fc7NmLOh2fzSNFaF9c9l8H9E=" #ashokSPNSecret 
+
+
+$pass = ConvertTo-SecureString $SPNSecret -AsPlainText –Force
+$cred = New-Object -TypeName pscredential –ArgumentList $SPNID, $pass
+Logout-AzureRmAccount  -ErrorAction SilentlyContinue
+Get-AzureRmContext 
+Login-AzureRmAccount -Credential $cred -ServicePrincipal –TenantId $tenantID 
+
+ 
 
 ### Create Resource Group
 New-AzureRmResourceGroup `
@@ -20,4 +35,5 @@ New-AzureRmResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
     -TemplateUri $template `
     -TemplateParameterUri $templateParameters `
-    -Verbose -Force
+    -Debug
+    
